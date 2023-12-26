36deb55cefb0675fac2e7e1e8c9b89999a5a1497
Arslan <arc1131@mail.ru>
2023-12-27 01:40:27 +0300

    add huawei push kit

--- app/src/main/java/com/example/unisafe/features/main/MainActivity.kt
+++ app/src/main/java/com/example/unisafe/features/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.unisafe.features.main
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import com.example.unisafe.R
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.common.ApiException
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +15,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getToken()
+    }
+
+    private fun getToken() {
+        // Create a thread.
+        object : Thread() {
+            override fun run() {
+                try {
+                    // Obtain the app ID from the agconnect-services.json file.  5765880207853176293
+                    val appId = "5765880207853176293"
+
+                    // Set tokenScope to HCM.
+                    val tokenScope = "HCM"
+                    val token = HmsInstanceId.getInstance(this@MainActivity).getToken(appId, tokenScope)
+                    Log.i(TAG, "get token:$token")
+
+                    // Check whether the token is null.
+                    if (!TextUtils.isEmpty(token)) {
+                        sendRegTokenToServer(token)
+                    }
+                } catch (e: ApiException) {
+                    Log.e(TAG, "get token failed, $e")
+                }
+            }
+        }.start()
+    }
+    private fun sendRegTokenToServer(token: String) {
+        Log.i(TAG, "sending token to server. token:$token")
     }
 }
